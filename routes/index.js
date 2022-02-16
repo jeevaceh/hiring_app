5b65c5fee4fff6074cd9300711ac5d8da415d875
jeevaceh <jeevaceh@gmail.com>
2022-02-16 15:57:27 +0530

    j

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,22 @@ const db = require("../model");
 const User = db.User;
 //post
 router.post('/job-detail', (req, res) => {
-    const data = {
-  profile:req.body.profile,
-  location:req.body.location,  
-  jobtype:req.body.jobtype,
-  about:req.body.about
-            };
-    // Save User in the database
-    User.create(data)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the user"
-        });
+  const data = {
+    profile: req.body.profile,
+    location: req.body.location,
+    jobtype: req.body.jobtype,
+    about: req.body.about
+  };
+  // Save User in the database
+  User.create(data)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the user"
       });
-  })
+    });
+})
 module.exports = router;
