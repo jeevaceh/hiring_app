d16c035e4b0aa7ef9093594ac0ad2cf1a2e48eb1
jeevaceh <jeevaceh@gmail.com>
2022-02-16 15:58:15 +0530

    j

--- model/jobdetails.js
+++ model/jobdetails.js
@@ -1,27 +1,27 @@
 
-module.exports = (sequelize, Sequelize)=>{
+module.exports = (sequelize, Sequelize) => {
 
-    //job detail model
+  //job detail model
 
-const User =sequelize.define('user',
-{
-   profile:{
-     type:Sequelize.DataTypes.STRING,
-     allowNull:false,
+  const User = sequelize.define('user',
+    {
+      profile: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
 
- },
-   location:{
-    type:Sequelize.DataTypes.STRING,
-     allowNull:false,
- },
-   jobtype:{
-    type:Sequelize.DataTypes.STRING,
-     allowNull:false,
- },
-   about:{
-    type:Sequelize.DataTypes.STRING(1234),
-    allowNull:false,
- }
-});
-return User;
+      },
+      location: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      jobtype: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      about: {
+        type: Sequelize.DataTypes.STRING(1234),
+        allowNull: false,
+      }
+    });
+  return User;
 }
--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const user = require("./model");
 user.sequelize.sync();
 
 app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization")
-      next();
-    });
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization")
+  next();
+});
 
- app.use('/api', require('./routes/index'));
+app.use('/api', require('./routes/index'));
 
 
-    const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log('The web server has started on port 8080');
+  console.log('The web server has started on port 8080');
 });
